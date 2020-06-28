047358cc74fa2f7f56f89967d6aa7244241193f1
Ken Frederick <ken.frederick@gmx.de>
2020-06-28 11:04:53 -0500

    Added user event callbacks

---
+++ example-event.lua
@@ -0,0 +1,23 @@
+local Passthrough = include("lib/passthrough")
+
+
+function midi_device_event(data)
+  local msg = midi.to_msg(data)
+  print("device")
+  tab.print(msg)
+end
+
+function midi_interface_event(data)
+  local msg = midi.to_msg(data)
+  print("interface")
+  tab.print(msg)
+end
+
+function init()
+    Passthrough.init()
+    Passthrough.user_device_event = midi_device_event
+    Passthrough.user_interface_event = midi_interface_event
+end
+
+function redraw()
+end
