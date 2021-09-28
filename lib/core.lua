dacbe499389d9f76e6f42ef0b484c468d9a7374e
Guy Purssell <38443772+nattog@users.noreply.github.com>
2021-09-28 18:12:21 +0100

    multiple targets (#11)
    * wip - menu demo
    
    * target refactor
    
    * tidy

--- lib/core.lua
+++ lib/core.lua
@@ -1,110 +1,180 @@
 local MusicUtil = require "musicutil"
 local pt_core = {}
 
-pt_core.params_added = false
-pt_core.device_channels = {"No change"}
-pt_core.interface_channels = {"Device src."}
+pt_core.midi_panic_active = false
+pt_core.input_channels = {"No change"}
+pt_core.output_channels = {"Device src."}
+pt_core.toggles = {"no", "yes"}
+pt_core.midi_ports = {}
+pt_core.midi_connections = {}
+pt_core.available_targets = {}
+pt_core.scales = {}
+local active_notes = {}
 
-for i = 1, 16 do
-    table.insert(pt_core.device_channels, i)
-    table.insert(pt_core.interface_channels, i)
+-- UTIL --
+local function get_channel_value(channel_param_value, msg_channel)
+    local channel_param= channel_param_value
+    return channel_param > 1 and (channel_param - 1) or msg_channel
 end
 
+-- SCALE SETUP --
 pt_core.scale_names = {}
 for i = 1, #MusicUtil.SCALES do
     table.insert(pt_core.scale_names, string.lower(MusicUtil.SCALES[i].name))
 end
 
-pt_core.cc_directions = {"D --> I", "D <--> I"}
-pt_core.toggles = {"no", "yes"}
-
-pt_core.build_scale = function(root, scale)
-    return MusicUtil.generate_scale_of_length(root, scale, 128)
+pt_core.build_scale = function(root, scale, index)
+    pt_core.scales[index] = MusicUtil.generate_scale_of_length(root, scale, 128)
 end
 
-pt_core.get_midi_devices = function()
-    d = {}
-    for id, device in pairs(midi.vports) do
-        d[id] = device.name
-    end
-    return d
+-- MIDI DEVICE DETECTION --
+for i = 1, 16 do
+    table.insert(pt_core.input_channels, i)
+    table.insert(pt_core.output_channels, i)
 end
 
-pt_core.device_event = function(midi_interface, device_channel, interface_channel, quantize_midi, current_scale, data)
-    if #data == 0 then
-          print('no data')
-          return
-    end
-    local msg = midi.to_msg(data)
-    local dev_channel_param = device_channel
-    local dev_chan = dev_channel_param > 1 and (dev_channel_param - 1) or msg.ch
-
-    local out_ch_param = interface_channel
-    local out_ch = out_ch_param > 1 and (out_ch_param - 1) or msg.ch
-
-    if msg and msg.ch == dev_chan then
-        local note = msg.note
-
-        if msg.note ~= nil then
-            if quantize_midi then
-                note = MusicUtil.snap_note_to_array(note, current_scale)
-            end
-        end
-
-        if msg.type == "note_off" then
-            midi_interface:note_off(note, 0, out_ch)
-        elseif msg.type == "note_on" then
-            midi_interface:note_on(note, msg.vel, out_ch)
-        elseif msg.type == "key_pressure" then
-            midi_interface:key_pressure(note, msg.val, out_ch)
-        elseif msg.type == "channel_pressure" then
-            midi_interface:channel_pressure(msg.val, out_ch)
-        elseif msg.type == "pitchbend" then
-            midi_interface:pitchbend(msg.val, out_ch)
-        elseif msg.type == "program_change" then
-            midi_interface:program_change(msg.val, out_ch)
-        elseif msg.type == "cc" then
-            midi_interface:cc(msg.cc, msg.val, out_ch)
+pt_core.setup_midi = function()
+    local midi_ports={}
+    local midi_connections = {}
+    local available_targets = {"all"}
+    for _,dev in pairs(midi.devices) do
+        if dev.port~=nil then
+            table.insert(midi_ports,dev.name)
+            local conn=midi.connect(dev.port)
+            table.insert(midi_connections, conn)
         end
     end
-end
 
-
-pt_core.interface_event = function(midi_device, device_channel, clock_device, cc_direction, data)
-    local msg = midi.to_msg(data)
-    local note = msg.note
-  
-    if clock_device then
-        if msg.type == "clock" then
-            midi_device:clock()
-        elseif msg.type == "start" then
-            midi_device:start()
-        elseif msg.type == "stop" then
-            midi_device:stop()
-        elseif msg.type == "continue" then
-            midi_device:continue()
-        end
+    pt_core.midi_ports = midi_ports
+    pt_core.midi_connections = midi_connections
+    for i = 1, tab.count(midi_ports) do
+        table.insert(available_targets, i)
     end
-    if cc_direction == 2 then
-        local dev_channel_param = device_channel
-        local dev_chan = dev_channel_param > 1 and (dev_channel_param - 1) or msg.ch
-  
-        if msg.type == "cc" then
-            midi_device:cc(msg.cc, msg.val, dev_chan)
-        end
-    end
+    pt_core.available_targets = available_targets
 end
 
 pt_core.root_note_formatter = function(value)
-  return MusicUtil.note_num_to_name(value)
+    return MusicUtil.note_num_to_name(value)
 end
 
-pt_core.user_device_event = function(data)
-  print('>> user-device-event <<')
+-- EVENTS ON MENU CHANGE --
+pt_core.remove_active_note = function(target, note, ch)
+    local i = 1
+    while i <= #active_notes do
+        if active_notes[i][1] == target and active_notes[i][2] == note and active_notes[i][3] == ch then
+            table.remove(active_notes, i)
+        end
+        i = i+1
+    end
 end
 
-pt_core.user_interface_event = function(data)
-  print('>> user-interface-event <<')
+pt_core.stop_clocks = function(origin, device_target)
+    local target_all = device_target == 1
+    local msg = {type="stop"}
+    if target_all then
+        for target = 1, #pt_core.midi_ports do
+            if origin ~= target then
+                pt_core.handle_clock_data(msg, pt_core.midi_connections[target])
+            end
+        end
+    elseif origin ~= device_target then
+            pt_core.handle_clock_data(msg, pt_core.midi_connections[device_target -1])
+    end
+end
+
+pt_core.stop_all_notes = function()
+    if #active_notes then
+        for i=1, #active_notes do
+            active_notes[i][1]:note_off(active_notes[i][2], 0, active_notes[i][3])
+        end
+    end
+    active_notes= {}
+end
+
+-- DATA HANDLERS --
+pt_core.handle_midi_data = function(msg, target, out_ch, quantize_midi, current_scale)
+    local note = msg.note
+
+    if note ~= nil then
+        if quantize_midi then
+            note = MusicUtil.snap_note_to_array(note, current_scale)
+        end
+    end
+
+    if msg.type == "note_off" then
+        target:note_off(note, 0, out_ch)
+        pt_core.remove_active_note(target, note, out_ch)
+    elseif msg.type == "note_on" then
+        target:note_on(note, msg.vel, out_ch)
+        table.insert(active_notes,{target,note,out_ch})
+    elseif msg.type == "key_pressure" then
+        target:key_pressure(note, msg.val, out_ch)
+    elseif msg.type == "channel_pressure" then
+        target:channel_pressure(msg.val, out_ch)
+    elseif msg.type == "pitchbend" then
+        target:pitchbend(msg.val, out_ch)
+    elseif msg.type == "program_change" then
+        target:program_change(msg.val, out_ch)
+    elseif msg.type == "cc" then
+        target:cc(msg.cc, msg.val, out_ch)
+    end
+end
+
+pt_core.handle_clock_data = function(msg, target)
+    if msg.type == "clock" then
+        target:clock()
+    elseif msg.type == "start" then
+        target:start()
+    elseif msg.type == "stop" then
+        target:stop()
+    elseif msg.type == "continue" then
+        target:continue()
+    end
+end
+
+pt_core.device_event = function(origin, device_target, input_channel, output_channel, send_clock, quantize_midi, current_scale, data)
+    if #data == 0 then
+        print('no data')
+        return
+    end
+    local msg = midi.to_msg(data)
+
+    local in_chan = get_channel_value(input_channel, msg.ch)
+    local out_ch = get_channel_value(output_channel, msg.ch)
+
+    -- should data target all ports
+    local target_all = device_target == 1 
+
+    if msg and msg.ch == in_chan then
+        -- get scale stored in scales object
+        local scale = pt_core.scales[origin]
+        if target_all then
+            for target = 1, #pt_core.midi_ports do
+                if origin ~= target then
+                    pt_core.handle_midi_data(msg, pt_core.midi_connections[target], out_ch, quantize_midi, scale)
+                end
+            end
+        elseif origin ~= device_target then
+            pt_core.handle_midi_data(msg, pt_core.midi_connections[device_target - 1], out_ch, quantize_midi, scale)
+        end
+    end
+
+    if send_clock then
+        if target_all then
+            for target = 1, #pt_core.midi_ports do
+                if origin ~= target then
+                    pt_core.handle_clock_data(msg, pt_core.midi_connections[target])
+                end
+            end
+        elseif origin ~= device_target then
+                pt_core.handle_clock_data(msg, pt_core.midi_connections[device_target -1])
+        end
+    end
+end
+
+pt_core.user_event = function(data, origin)
+    -- local msg = midi.to_msg(data)
+    -- print(origin.port .. ' ' .. origin.name .. ' ' .. msg.type)
 end
 
 return pt_core
--- lib/mod.lua
+++ lib/mod.lua
@@ -1,147 +1,59 @@
 local mod = require 'core/mods'
 
 local core = require("passthrough/lib/core")
-tab = require "tabutil"
-
-local devices = {}
-local clock_device
-local quantize_midi
-local current_scale = {}
-local midi_device
-local midi_interface
+local tab = require "tabutil"
 
 local api = {}
-local running = false
+local clock_messages = {"clock", "start", "stop", "continue"}
+local config = {}
+local state = {}
 
-api.user_device_event = core.user_device_event
-api.user_interface_event = core.user_interface_event
-
-local state = {
-  midi_device = 1,
-  device_channel=1,
-  midi_interface = 2,
-  interface_channel=1,
-  clock_device = 1,
-  quantize_midi = 1,
-  current_scale = 1,
-  cc_direction = 1,
-  root_note = 0,
-  post_startup = false
-}
-
-local passthrough_config = {
-  midi_device = {
-    param_type = "option",
-    id = "midi_device",
-    name = "Midi Device",
-    options = devices,
-    action = function(value)
-        if midi_device then
-          midi_device.event = nil
-        end
-        midi_device = midi.connect(value)
-        midi_device.event = device_event
-    end,
-    formatter = function(value)
-      return devices[value]
-    end
-  },
-  midi_interface = {
-    param_type = "option",
-    id = "midi_interface",
-    name = "Midi Interface",
-    options = devices,
-    action = function(value)
-        if midi_interface then
-          midi_interface.event = nil
-        end
-        midi_interface = midi.connect(value)
-        midi_interface.event = interface_event
-    end,
-    formatter = function(value)
-      return devices[value]
-    end
-  },
-  device_channel = {
-    param_type = "option",
-    id = "device_channel",
-    name = "Device channel",
-    options = core.device_channels
-  },
-  interface_channel = {
-    param_type = "option",
-    id = "interface_channel",
-    name = "Interface channel",
-    options = core.interface_channels
-  },
-  cc_direction = {    
-    param_type = "option",
-    id = "cc_direction",
-    name = "CC msg direction",
-    options = core.cc_directions
-  },
-  clock_device = {
-    param_type = "option",
-    id = "clock_device",
-    name = "Clock device",
-    options = core.toggles,
-    action = function(value)
-        clock_device = value == 2
-        if value == 1 then
-            midi_device:stop()
-        end
-    end
-    },
-  quantize_midi = {
-    param_type = "option",
-    id = "quantize_midi",
-    name = 'Quantize midi',
-    options = core.toggles,
-    action = function(value)
-        quantize_midi = value == 2
-        current_scale = core.build_scale(state.root_note, state.current_scale)
-    end
-  },
-  root_note = {
-    param_type = 'number',
-    id = 'root_note',
-    name = "Root note",
-    minimum = 0,
-    maximum = 11,
-    formatter = core.root_note_formatter,
-    action = function()
-        current_scale = core.build_scale(state.root_note, state.current_scale)
-    end
-  },
-  current_scale = {
-      param_type = 'option',
-      id = 'current_scale',
-      name = 'Current scale',
-      options = core.scale_names,
-      action = function()
-        current_scale = core.build_scale(state.root_note, state.current_scale)
-      end
-    }
-}
+-- NORNS OVERRIDES --
 
 local midi_add = _norns.midi.add
+local midi_remove = _norns.midi.remove
+local midi_connect = _norns.midi.connect
+local script_clear = norns.script.clear
 
 _norns.midi.add = function(id, name, dev)
   midi_add(id, name, dev)
   update_devices()
 end
 
-local midi_remove = _norns.midi.remove
-
 _norns.midi.remove = function(id)
   midi_remove(id)
   update_devices()
 end
 
-local midi_connect = _norns.midi.connect
-
 _norns.midi.connect = function(id)
     midi_connect(id)
+end
+
+norns.script.clear = function()
+  script_clear()
+  launch_passthrough()
+end
+
+-- STATE FUNCTIONS --
+function write_state()
+  local f = io.open(_path.data..'passthrough.state',"w+")
+  io.output(f)
+  io.write("return {")
+  for i =1, tab.count(state) do
+    local port_config = state[i]
+    if i~=1 then
+      io.write(",")
+    end
+    io.write("{ target="..port_config.target..",")
+    io.write("input_channel="..port_config.input_channel..",")
+    io.write("output_channel="..port_config.output_channel..",")
+    io.write("send_clock="..port_config.send_clock..",")
+    io.write("quantize_midi="..port_config.quantize_midi..",")
+    io.write("current_scale="..port_config.current_scale..",")
+    io.write("root_note="..port_config.root_note.."}")
+  end
+  io.write("}\n")
+  io.close(f)
 end
 
 function read_state() 
@@ -151,233 +63,216 @@ function read_state()
     state = dofile(_path.data..'passthrough.state')
   end
 
-  current_scale = core.build_scale(state.root_note, state.current_scale)
+  for i = 1, tab.count(state) do
+    core.build_scale(state[i].root_note, state[i].current_scale, i)
+  end
 end
 
+function assign_state()
+  for i=1, tab.count(config) do
+    if state[i] then
+      for k, v in ipairs(state[i]) do
+        config[k].action(v)
+      end
+    end
+  end
+end
+
+-- HOOKS --
 mod.hook.register("system_post_startup", "read passthrough state", function()
   read_state()
-  update_devices()
+  launch_passthrough()
 end)
 
 mod.hook.register("system_pre_shutdown", "write passthrough state", function()
-  local f = io.open(_path.data..'passthrough.state',"w+")
-  io.output(f)
-  io.write("return { midi_device="..state.midi_device..',')
-  io.write("device_channel="..state.device_channel..',')
-  io.write("midi_interface="..state.midi_interface..',')
-  io.write("interface_channel="..state.interface_channel..',')
-  io.write("clock_device="..state.clock_device..',')
-  io.write("quantize_midi="..state.quantize_midi..',')
-  io.write("current_scale="..state.current_scale..",")
-  io.write("cc_direction="..state.cc_direction..",")
-  io.write("root_note="..state.root_note.." }\n")
-  io.close(f)
+  write_state()
 end)
 
 mod.hook.register("script_post_cleanup", "passthrough post cleanup", function()
   launch_passthrough()
+  api.user_event = core.user_event
 end)
-
-function device_event(data)
-    core.device_event(midi_interface, state.device_channel, state.interface_channel, state.quantize_midi, current_scale, data)
-    api.user_device_event(data)
-end
-
-function interface_event(data)
-  print('raw data from interface')
-    core.interface_event(midi_device, state.device_channel, state.clock_device, state.cc_direction, data)
-    api.user_interface_event(data)
-end
-
-function update_devices() 
-  devices=core.get_midi_devices()
-  passthrough_config.midi_device.options = devices
-  passthrough_config.midi_interface.options = devices
-
-end
-
-function launch_passthrough()
-    running = true
-    -- ensure devices is up to date for device options menu
-    update_devices()
-
-    -- connect state devices
-    passthrough_config.midi_device.action(state.midi_device)
-    passthrough_config.midi_interface.action(state.midi_interface)
-    print("-- passthru ready --")
-end
-
-function add_params()
-    params:add_group("PASSTHROUGH", 9)
-    params:add {
-        type = "option",
-        id = "midi_device",
-        name = "Device",
-        options = devices,
-        default = state.midi_device,
-        action = function(value)
-            state.midi_device = value
-            midi_device.event = nil
-            local existing_event = midi.vports[value].event and midi.vports[value].event or nil
-            midi_device = midi.connect(value)
-            midi_device.event = function(data) 
-              if existing_event then
-                existing_event(data)
-              end
-              device_event(data)
-            end
-        end
-    }
-    params:add {
-        type = "option",
-        id = "midi_interface",
-        name = "Interface",
-        options = devices,
-        default = state.midi_interface,
-        action = function(value)
-            state.midi_interface = value
-            midi_interface.event = nil
-            local existing_event = midi.vports[value].event and midi.vports[value].event or nil
-            midi_interface = midi.connect(value)
-            midi_interface.event = function(data) 
-              if existing_event then
-                existing_event(data)
-              end
-              interface_event(data) 
-            end
-        end
-    }
-    params:add {
-        type = "option",
-        id = "cc_direction",
-        name = "CC msg direction",
-        options = core.cc_directions,
-        default = state.cc_direction,
-        action = function(value)
-          state.cc_direction = value
-        end
-    }
-    params:add {
-        type = "option",
-        id = "device_channel",
-        name = "Device channel",
-        options = core.device_channels,
-        default = state.device_channel,
-        action = function(value)
-          state.device_channel = value
-        end
-    }
-    params:add {
-        type = "option",
-        id = "interface_channel",
-        name = "Interface channel",
-        options = core.interface_channels,
-        default = state.interface_channel,
-        action = function(value)
-          state.interface_channel = value
-        end
-    }
-    params:add {
-        type = "option",
-        id = "clock_device",
-        name = "Clock device",
-        options = core.toggles,
-        action = function(value)
-            state.clock_device = value
-            clock_device = value == 2
-            if value == 1 then
-                midi_device:stop()
-            end
-        end
-    }
-    params:add {
-        type = "option",
-        id = "quantize_midi",
-        name = "Quantize",
-        options = {"no", "yes"},
-        action = function(value)
-            state.quantize_midi = value
-            quantize_midi = value == 2
-            current_scale = core.build_scale(state.root_note, state.current_scale)
-        end
-    }
-    
-    params:add {
-        type = "number",
-        id = "root_note",
-        name = "Root",
-        min = 0,
-        max = 11,
-        default = 0,
-        formatter = function(param) 
-          return core.root_note_formatter(param:get())
-        end,
-        action = function(value)
-            state.root_note = value
-            current_scale = core.build_scale(state.root_note, state.current_scale)
-        end
-        
-    }
-
-    params:add {
-        type = "option",
-        id = "current_scale",
-        name = "Current Scale",
-        options = core.scale_names,
-        default = 5,
-        action = function(value)
-            state.current_scale = value
-            current_scale = core.build_scale(state.root_note, state.current_scale)
-        end
-    }
-
-    core.params_added = true
-end
 
 mod.hook.register("script_pre_init", "passthrough", function()
   -- tweak global environment here ahead of the script `init()` function being called
   local script_init = init
   
   init = function()
-      add_params()
       script_init()
       launch_passthrough()
   end
 end)
 
-local m = {}
 
-local screen_order = {{"midi_device", "midi_interface", "device_channel", "interface_channel", "clock_device", "cc_direction"}, {'quantize_midi', 'root_note', 'current_scale'}}
+-- ACTIONS + EVENTS --
+function create_config()
+  local config={}
+  for port = 1, tab.count(core.midi_ports) do
+    -- if no state exists for this port, create a new one
+    if state[port] == nil then
+      print('no state saved for port, adding defaults')
+      state[port] = {
+        target = 1,
+        input_channel = 1,
+        output_channel = 1,
+        send_clock = 1,
+        quantize_midi = 1,
+        current_scale = 1,
+        root_note = 0
+      }
+    end
+    
+    -- config creates an object for each passthru parameter
+    config[port] = {
+      target = {
+        param_type = "option",
+        id = "target",
+        name = "Target",
+        options = core.available_targets,
+        action = function(value)
+          local existing_event = core.midi_connections[port].event
+          core.midi_connections[port].event = function(data) 
+            device_event(data, port)
+          end
+        end,
+        formatter = function(value)
+          return value == 1 and core.available_targets[value] or core.midi_ports[value-1]
+        end
+      },
+      input_channel = {
+        param_type = "option",
+        id = "input_channel",
+        name = "Input channel",
+        options = core.input_channels
+      },
+      output_channel = {
+        param_type = "option",
+        id = "output_channel",
+        name = "Output channel",
+        options = core.output_channels
+      },
+      send_clock = {
+        param_type = "option",
+        id = "send_clock",
+        name = "Clock out",
+        options = core.toggles,
+        action = function(value)
+            if value == 1 then
+                core.stop_clocks(origin, state[port].target)
+            end
+        end
+        },
+      quantize_midi = {
+        param_type = "option",
+        id = "quantize_midi",
+        name = 'Quantize midi',
+        options = core.toggles
+      },
+      root_note = {
+        param_type = 'number',
+        id = 'root_note',
+        name = "Root",
+        minimum = 0,
+        maximum = 11,
+        formatter = core.root_note_formatter,
+        action = function()
+            core.build_scale(state[port].root_note, state[port].current_scale, port)
+        end
+      },
+      current_scale = {
+          param_type = 'option',
+          id = 'current_scale',
+          name = 'Scale',
+          options = core.scale_names,
+          action = function()
+            core.build_scale(state[port].root_note, state[port].current_scale, port)
+          end
+        }
+    }
 
-local screen_delta = 1
-local page = 1
+    config[port].target.action(state[port].target)
+    config[port].root_note.action(state[port].root_note, state[port].current_scale, port)
+    config[port].current_scale.action(state[port].root_note, state[port].current_scale, port)
+  end
 
-function update_parameter(p, dir)
+  return config
+end
+
+function device_event(data, origin)
+    core.device_event(
+      origin,
+      state[origin].target,
+      state[origin].input_channel,
+      state[origin].output_channel,
+      state[origin].send_clock,
+      state[origin].quantize_midi,
+      state[origin].current_scale,
+      data)
+
+    -- filter unwanted clock events
+    if state[origin].send_clock == 1 and #data and tab.contains(clock_messages, midi.to_msg(data).type) then return end
+    api.user_event(data, {name= core.midi_ports[origin], port= origin})
+end
+
+function update_devices() 
+  core.setup_midi()
+  config = create_config()
+  assign_state()
+end
+
+function launch_passthrough()
+    update_devices()
+end
+
+function update_parameter(p, index, dir)
   -- update options
   if p.param_type == "option" then
-    state[p.id] = util.clamp(state[p.id] + dir, 1, #p.options)
+    state[index][p.id] = util.clamp(state[index][p.id] + dir, 1, #p.options)
   end
 
   -- generate scale
   if p.param_type == 'number' then
-    state[p.id] = util.clamp(state[p.id] + dir, p.minimum, p.maximum)
+    state[index][p.id] = util.clamp(state[index][p.id] + dir, p.minimum, p.maximum)
   end
-  
+
   if p.action and type(p.action == 'function') then
-    p.action(state[p.id])
+    p.action(state[index][p.id])
   end
+
+  write_state()
 end
 
-function format_parameter(p) 
+function format_parameter(p, index) 
   if p.formatter and type(p.formatter == 'function') then
-    return p.formatter(state[p.id])
+    return p.formatter(state[index][p.id])
   end
 
   if p.param_type == "option" then
-    return p.options[state[p.id]]
+    return p.options[state[index][p.id]]
   end
 
-  return state[p.id]
+  return state[index][p.id]
+end
+
+
+-- MOD MENU --
+local screen_order = {"target", "input_channel", "output_channel", "send_clock", 'quantize_midi', 'root_note', 'current_scale', 'midi_panic'}
+local m = {
+  list=screen_order,
+  pos=0,
+  page=1,
+  len=tab.count(screen_order),
+  show_hint = true,
+  display_panic = false
+}
+
+local toggle_display_panic = function()
+  clock.run(function()
+      m.display_panic=true
+      clock.sleep(0.5)
+      m.display_panic=false
+      mod.menu.redraw()
+  end)
 end
 
 m.key = function(n, z)
@@ -385,48 +280,92 @@ m.key = function(n, z)
     mod.menu.exit()
   end
   if n == 3 and z == 1 then
-    page = page == 1 and 2 or 1
-    screen_delta = 1
+    m.page = util.wrap(m.page + z, 1, tab.count(config))
+    m.pos = 0
+    m.show_hint = false
     mod.menu.redraw()
   end
 end
 
 m.enc = function(n, d)
+  m.show_hint = false
   if n == 2 then
-    screen_delta = util.clamp(screen_delta + d, 1, #screen_order[page])
+    if m.pos == 0 and d == -1 then
+      m.show_hint = true
+    end
+    m.pos = util.clamp(m.pos + d, 0, m.len - 1)
   end
-  
+
   if n == 3 then
-    update_parameter(passthrough_config[screen_order[page][screen_delta]], d)
+    if m.list[m.pos+1] == 'midi_panic' then
+      core.stop_all_notes()
+      toggle_display_panic()
+    else
+      update_parameter(config[m.page][m.list[m.pos + 1]], m.page, d)
+    end
   end 
   mod.menu.redraw()
 end
 
 m.redraw = function()
   screen.clear()
-  for index, value in ipairs(screen_order[page]) do
-    screen.move(4, 10 * index)
-    local param = passthrough_config[value]
-    screen.level(index == screen_delta and 15 or 7)
-    screen.text(param.name .. " " .. format_parameter(param))
+  for i=1,6 do
+    if (i > 2 - m.pos) and (i < m.len - m.pos + 3) then
+      screen.move(0,10*i)
+      local line = m.list[i+m.pos-2]
+      if(i==3) then
+        screen.level(15)
+      else
+        screen.level(4)
+      end
+
+      if line == 'midi_panic' then
+        screen.text("Midi panic : ")
+        screen.rect(50, (10*i)-4.5, 5, 5)
+        screen.level(m.display_panic and 15 or 4)
+        screen.fill()
+      else
+        local param = config[m.page][line]
+        screen.text(param.name .. " : " .. format_parameter(param, m.page))
+      end
+    end
   end
-  
+  screen.rect(0, 0, 140, 13)
+  screen.level(0)
+  screen.fill()
+  screen.level(15)
+  screen.move(120, 10)
+  screen.text_right(string.upper(core.midi_ports[m.page]))
+  if m.show_hint then
+    screen.level(2)
+    screen.move(0, 20)
+    screen.text('E2 scroll')
+    screen.move(120, 20)
+    screen.text_right('E3 select')
+    screen.move(0, 10)
+    screen.text("K3 port")
+  end
   screen.update()
 end
 
 m.init = function() 
-  page = 1
-  screen_delta = 1
-  
+  m.page = 1
+  m.pos = 0
+  m.show_hint=true
   launch_passthrough()
 end
 
-m.deinit = function() end
+m.deinit = function() 
+  write_state()
+end
 
 mod.menu.register(mod.this_name, m)
 
+-- API --
 api.get_state = function()
   return state
 end
+
+api.user_event = core.user_event
 
 return api
